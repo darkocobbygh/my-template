598c329139059de71b7b63491641b3a3f52f5b33
darkocobbygh <darkogaucho@gmail.com>
2023-05-24 15:54:40 +0000

    First commit

---
+++ src/components/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Servies() {
+    return (
+        <>
+            <h1 className='services'>SERVICES</h1>
+        </>
+    )
+}
+
+
